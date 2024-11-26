802df3c48ad61b51b426aa2f856480f99f2672ab
algorubenjr <algoruben018@gmail.com>
2024-11-26 15:01:21 +0800

    Add files via upload

---
+++ StringConcatenation1.java
@@ -0,0 +1,45 @@
+public class StringConcatenation1 {
+public static String concatenateStrings(String str1, String str2) {
+
+        int len1 = str1.length();
+
+        int len2 = str2.length();
+
+        
+        char[] str3 = new char[len1 + len2];
+
+        
+        int i = 0;
+        while (i < len1) {
+
+            str3[i]= str1.charAt(i);
+
+            i++;
+
+        }
+
+        int j = 0;
+
+        while (i < len1+len2) {
+
+            str3[i]= str2.charAt(j);
+            i++;
+
+            j++;
+
+        }
+            return new String(str3);
+
+    }
+            public static void main(String[] args) {
+
+                String str1 = "Ruben Jr ";
+
+                String str2 = "Algo";
+
+                String concatenatedString = concatenateStrings(str1, str2);
+                System.out.println("Concatenated string:" + concatenatedString);
+
+}
+
+}
